c60dfeb20bfa84e9639ebef0477f9f639f137963
Lucas Barauna <82176900+LucasBara-dising@users.noreply.github.com>
2024-03-25 14:45:21 -0300

    Todos os sliders de ingredintes implementado, tela pronta

--- js/monteSeuLanche.js
+++ js/monteSeuLanche.js
@@ -1,22 +1,44 @@
 let itensSelectPao=[]
 let itensSelectQueijos=[]
+let itensSelectMolhos=[]
+let itensSelectSalada=[]
+let itensSelectHamburger=[]
 
 fetch('./json/sampleIngredintes.json').then((response) => {
 response.json().then((dados) => {
   //posso manipular o json aqui
   const sizeJson = dados.paes.length
   criaSlider("AddImgPao","btnLeftPao", "btnRightPao", "divImgPao", 
-  "infoTitlePao", "checkItemPao", itensSelectPao , dados.paes, sizeJson, 0)
+  "infoTitlePao", "checkItemPao", itensSelectPao , dados.paes, sizeJson, 0,1)
 
   //queijos
   const sizeJsonQueijos = dados.queijos.length
   criaSlider("AddImgQueijos","btnLeftQueijos", "btnRightQueijos", "divImgQueijos", 
   "infoTitleQueijos", "checkItemQueijos", itensSelectQueijos , dados.queijos, 
-  sizeJsonQueijos, 0)
+  sizeJsonQueijos, 0,1)
+
+  //Molhos
+  const sizeJsonMolhos = dados.molhos.length
+  criaSlider("AddImgMolhos","btnLeftMolhos", "btnRightMolhos", "divImgMolhos", 
+  "infoTitleMolhos", "checkItemMolhos", itensSelectMolhos , dados.molhos, 
+  sizeJsonMolhos, 0,2)
+
+  //Salada
+  const sizeJsonSalada = dados.salada.length
+  criaSlider("AddImgSalada","btnLeftSalada", "btnRightSalada", "divImgSalada", 
+  "infoTitleSalada", "checkItemSalada", itensSelectSalada , dados.salada, 
+  sizeJsonSalada, 0,3)
+
+  //Hamburger
+  const sizeJsonHamburger = dados.hamburger.length
+  criaSlider("AddImgHamburger","btnLeftHamburger", "btnRightHamburger", "divImgHamburger", 
+  "infoTitleHamburger", "checkItemHamburger", itensSelectHamburger , dados.hamburger, 
+  sizeJsonHamburger, 0,2)
+
   });
 })
 
-function criaSlider(divAddImg, btnLeft, btnRight, divImg, divTitle, imgMarkItem, arrayItemIngredient, objIngredient, sizeobjIngredient, currentImg){
+function criaSlider(divAddImg, btnLeft, btnRight, divImg, divTitle, imgMarkItem, arrayItemIngredient, objIngredient, sizeobjIngredient, currentImg, limiteItens){
   //passa o lugar dos carrosel e onde vai receber a img e titulo
   showItem(divImg, divTitle, imgMarkItem, arrayItemIngredient, objIngredient, currentImg)
 
@@ -27,14 +49,14 @@ function criaSlider(divAddImg, btnLeft, btnRight, divImg, divTitle, imgMarkItem,
     addImgs.addEventListener("click", function() {
       if(arrayItemIngredient.includes(currentImg)==true){
         removeByElement(arrayItemIngredient,currentImg)
-        checkItemSelect(arrayItemIngredient, currentImg, imgMarkItem)
+        
       }else{
-        if(arrayItemIngredient.length>=1){
+        if(arrayItemIngredient.length>=limiteItens){
           arrayItemIngredient=[]
         }
         arrayItemIngredient.push(currentImg)
-        checkItemSelect(arrayItemIngredient, currentImg, imgMarkItem)
       }
+      checkItemSelect(arrayItemIngredient, currentImg, imgMarkItem)
       console.log(arrayItemIngredient)
     });
 
