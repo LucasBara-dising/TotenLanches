293577838da47e666801a971ceeed2394c7106c6
Lucas Barauna <82176900+LucasBara-dising@users.noreply.github.com>
2024-04-15 13:55:04 -0300

    tela de bebidas agora suporta receber iten de um json e outras melhorias

--- js/bebidas.js
+++ js/bebidas.js
@@ -1,41 +1,142 @@
+let qtnSuc = 0
+let qtnRefri= 0
+let qtnAgua = 0
+let totalValor=0
 
-// ---- Seleção de itens, carrinho de compras e cálculo de valor ca compra -----------
+var valor_suco 
+var valor_refri
+var valor_agua
 
+// ---- Seleção de itens, carrinho de compras e cálculo de valor ca compra -----------    
 document.addEventListener('DOMContentLoaded', function () {
-    let selectedItems = []; // Array para armazenar os itens selecionados pelo usuário
-    let cards = document.querySelectorAll(".col-4");
-    let existingCheck;
+    fetch('./json/bebidasRepouse.json').then((response) => {
+        response.json().then((dados) => {
+          //posso manipular o json aqui
 
-    cards.forEach((card) => {
-        card.addEventListener('click', handleClick)
+        valor_suco = dados.valor_suco
+        valor_refri = dados.valor_refri
+        valor_agua = dados.valor_agua
+    
+          for(i=0; i<dados.sucos.length; i++){
+            criaCard(dados.sucos, i, valor_suco, "conteinerSucos", "Suco")
+          }
+
+          for(i=0; i<dados.refrigerantes.length; i++){
+            criaCard(dados.refrigerantes, i, valor_refri, "conteinerRefri", "Refrigerante")
+          }
+
+          for(i=0; i<dados.agua.length; i++){
+            criaCard(dados.agua, i, valor_agua, "conteinerAgua", "Agua")
+          }
+        });
     })
+
+    let selectedItems = [];// Array para armazenar os itens selecionados pelo usuário
+    let QtnSelectedItems = new Map();
+    
+    QtnSelectedItems.set('Suco', 0);
+    QtnSelectedItems.set('Refrigerante', 0);
+    QtnSelectedItems.set('Agua', 0); 
+
+    function removeByElement(array, item){
+        const index = array.indexOf(item);
+        if (index > -1) { 
+          array.splice(index, 1); 
+        }
+    }
+
+    function criaCard(objItem, i, valor_item, conteiner, categoria){
+        let box = document.createElement('div');
+        box.className="col-4 mt-3"
+        box.id=objItem[i].nome
+        
+        let boxImg = document.createElement('div');
+        boxImg.className="card-body"
+  
+        let img = document.createElement('img');
+        img.className="img-fluid"
+        img.src=objItem[i].imagem_url
+
+        let imgCkeck = document.createElement('img');
+        imgCkeck.className="check"
+        imgCkeck.src="./img/check.jpg"
+        imgCkeck.style.visibility="hidden"
+
+        let textItem = document.createElement('H6');
+        textItem.className="card-footer mt-3 text-center"
+        textItem.innerHTML= objItem[i].nome
+        
+        const conteinerItem = document.getElementById(conteiner);
+        conteinerItem.appendChild(box)
+
+        box.onclick=function(){  
+            checkItem(this.id, imgCkeck, categoria, valor_item)
+        }
+
+        box.appendChild(boxImg)
+        boxImg.appendChild(img)
+        boxImg.appendChild(imgCkeck)
+
+        box.appendChild(textItem)
+    }
+
+    function checkItem(idBox, imgCkeck, categoria){
+        if(selectedItems.includes(idBox)==true){
+            imgCkeck.style.visibility="hidden"
+            removeByElement(selectedItems, idBox)
+            switch(categoria){
+                case "suco":
+                    qtnSuc--
+                    QtnSelectedItems.set(categoria, qtnSuc)
+                    break;
+                case "Refrigerante": 
+                    qtnRefri--
+                    QtnSelectedItems.set(categoria, qtnRefri)
+                    break;
+                case "Agua":
+                    qtnAgua--
+                    QtnSelectedItems.set(categoria, qtnAgua)
+                    break;
+                default:
+                    qtnSuc--
+                    QtnSelectedItems.set(categoria, qtnSuc)
+                   
+            }
+            
+        }else{
+            imgCkeck.style.visibility="visible"
+            selectedItems.push(idBox)
+            saveName();
+            switch(categoria){
+                case "suco":
+                    qtnSuc++
+                    QtnSelectedItems.set(categoria, qtnSuc)
+                    break;
+                case "Refrigerante": 
+                    qtnRefri++
+                    QtnSelectedItems.set(categoria, qtnRefri)
+                    break;
+                case "Agua":
+                    qtnAgua++
+                    QtnSelectedItems.set(categoria, qtnAgua)
+                    break;
+                default:
+                    qtnSuc++
+                    QtnSelectedItems.set(categoria, qtnSuc)
+            }
+        }
+        total()
+
+        let txtTotalPrice = document.getElementById('txtTotal'); //elemento onde será exibido o preço total
+        txtTotalPrice.textContent = "R$: " + total().toFixed(2);
+    }
+
 
     const carrinho = document.getElementById("addToCartButton");
     carrinho.addEventListener("click", function() {
         //funcion na pagina criaJsonGreal
-        geraTextIngrientes(dados.paes, dados.queijos, dados.molhos, dados.salada, dados.extra, dados.hamburger,valorPedido)
+        //geraTextIngrientes(dados.paes, dados.queijos, dados.molhos, dados.salada, dados.extra, dados.hamburger,valorPedido)
     })
-
-    function handleClick(){
-        existingCheck = this.querySelector('.check'); //'this' é cada card selecionado
-        let itemName = this.querySelector('.card-footer').textContent;
-        let itemAddCheck = this.querySelector('.card-body');
-
-        if (existingCheck) { // Se o item já estiver selecionado, remova-o da lista
-            itemAddCheck.removeChild(existingCheck);
-            selectedItems = selectedItems.filter(item => item.name !== itemName);//vai manter no array somente elementos com nomes diferentes
-        } else { // Se o item não estiver selecionado, adicione-o à lista
-            let checkClone = document.createElement('img');
-            checkClone.src = "./img/check.jpg";
-            checkClone.classList.add('check');
-
-            itemAddCheck.appendChild(checkClone);
-           
-            if(!selectedItems.some(item => item.name === itemName)){
-                selectedItems.push({ name: itemName });
-            }
-        }
-    };
 
     // função para guardar os itens (nomes) no carrinho 
     function saveName(){
@@ -44,7 +145,7 @@ document.addEventListener('DOMContentLoaded', function () {
         cartItems.innerHTML = "";
         selectedItems.forEach(item=>{
             let list = document.createElement('li');
-            let textContent= "1x "+item.name
+            let textContent= "1x "+item
             let itemName = document.createTextNode(textContent);
             list.style.display = "flex";
             list.style.marginTop = "10px";
@@ -52,67 +153,18 @@ document.addEventListener('DOMContentLoaded', function () {
             cartItems.appendChild(list);
         })     
     }
+
     //função para informar a quantidade de produtos do carrinho 
-    //será chamada no evento de click de cada card.
-    function quantityOfProducts(){
-        let quantityOfProducts = document.querySelector('.quantityOfProducts');
-        quantityOfProducts.innerHTML = selectedItems.length;
-        quantityOfProducts.style.color = 'whitesmoke';
-        quantityOfProducts.top = '15%';
-        if(selectedItems.length == 0){
-            quantityOfProducts.innerHTML = '';
-        }
+    //TO DO alterar quantidade
+
+    function total(){
+        return (QtnSelectedItems.get("Suco") * valor_suco) + (QtnSelectedItems.get("Refrigerante") * valor_refri) + (QtnSelectedItems.get("Agua") * valor_agua)
     }
-
-
-    let totalPrice = document.getElementById('totalPrice'); // Seleciona o elemento onde será exibido o preço total
-    let selectedItemsTotal = 0; // Inicializa o total dos itens selecionados como zero
-          
-    // Adiciona um evento de clique a cada card
-    cards.forEach(card => {
-        card.addEventListener('click', () => {
-            let input = card.querySelector('input[name="prices"]'); // Seleciona o input correspondente ao card clicado
-            let price = parseFloat(input.value); // Obtém o valor do input convertido para número
-            
-            if (card.classList.contains('selected')) { // Se o card já estiver selecionado, subtrai o valor do total
-                selectedItemsTotal -= price;  
-            } else { // Se o card não estiver selecionado, ao ser clicado, será selecionado e adicionado o valor ao total
-                selectedItemsTotal += price;   
-            }
-
-            card.classList.toggle('selected'); // Adiciona ou remove a classe 'selected' do card
-            saveName();
-            quantityOfProducts();
-        });
-
-    });
-
+ 
     // Adiciona um evento de clique ao botão 'Add ao Carrinho'
     document.getElementById('addToCartButton').addEventListener('click', () => {
         // Atualiza o texto dentro do elemento totalPrice com o total dos itens selecionados
-        totalPrice.textContent = "Total R$ " + selectedItemsTotal.toFixed(2);
+        let totalPrice = document.getElementById('totalPrice'); //elemento onde será exibido o preço total
+        totalPrice.textContent = "Total R$ " + total().toFixed(2);
     });
-
-    // Mostra os itens do carrinho com o total da compra
-    let images = document.querySelectorAll('.show');
-    let checkout = document.querySelector('.checkout')
-    
-    images.forEach(image=>{
-        console.log(images)
-        image.addEventListener('click', ()=>{
-            checkout.classList.toggle('visible');
-            if(checkout.classList.contains('visible')){
-            checkout.style.width = '61vh';
-            checkout.style.height = '90vh';
-            checkout.style.left = '13%';
-            checkout.style.top = '0%';
-            checkout.style.color = 'whitesmoke';
-            checkout.style.backgroundColor = 'rgb(35,40,43,.98';
-            checkout.style.display = 'block';
-            }else{
-                checkout.style.display = 'none';
-            }
-        })
-    })
-
 })
